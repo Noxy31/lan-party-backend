54480e9183f5f873f4a02195a7718602a75eb204
Noxy31 <nellenoar@gmail.com>
2024-07-01 10:46:19 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world 2");
+import express from 'express';
+import { join } from 'path';
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(join(__dirname, '../../lan-party-frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '../../lan-party-frontend/dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
